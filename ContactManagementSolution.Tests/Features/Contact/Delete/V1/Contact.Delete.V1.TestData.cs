4d4c3daad922ebf3145e4a73abd592a1fb289ba9
Jacob Timms <jacob_timms@live.com>
2025-03-10 10:35:13 +0000

    Remove .gitignore.bak from the project

--- ContactManagementSolution.Tests/Features/Contact/Delete/V1/Contact.Delete.V1.TestData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ContactManagementSolution.Tests.Features.Contact.Delete.V1;
-
-public class TestData
-{
-    public static readonly Entities.Contact Contact = new()
-    {
-        Id = Guid.NewGuid(), Name = "Jane Smith", Email = "jane.smith@example.com", PhoneNumber = "098-765-4321"
-    };
-}
